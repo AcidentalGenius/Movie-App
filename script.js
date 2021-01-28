d9700c83830cd185deb300af14576a05f92a6264
AcidentalGenius <78159163+AcidentalGenius@users.noreply.github.com>
2021-01-28 13:24:13 -0300

    Each movie is now a link to the page of itself

--- script.js
+++ script.js
@@ -29,18 +29,19 @@ function showMovies(movies) {
         
 
         movieEl.innerHTML = `
-            <img src="${IMG_PATH + poster_path}">
+            <a href="movie.html?=${title}">
+                <img src="${IMG_PATH + poster_path}">
 
-            <div class="movie-info">
-                <h3>${title}</h3>
-                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-            </div>
+                <div class="movie-info">
+                    <h3>${title}</h3>
+                    <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+                </div>
 
-            <div class="overview">
-                <h3>Overview</h3>
-                ${overview}
-                <a href="movie.html?=${title}">${title}</a>
-            </div>
+                <div class="overview">
+                    <h3>Overview</h3>
+                    ${overview}
+                </div>
+            </a>
         `;
 
         main.appendChild(movieEl);
